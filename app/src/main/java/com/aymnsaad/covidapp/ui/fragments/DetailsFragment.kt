84b20593a24d0e742bcb5a0b21cdf8aa7026f42f
Nooralden <nooraldenakel@gmail.com>
2021-11-23 04:14:38 +0300

    feat:add ViewPagerAdapter

--- app/src/main/java/com/aymnsaad/covidapp/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/aymnsaad/covidapp/ui/fragments/DetailsFragment.kt
@@ -3,11 +3,16 @@ package com.aymnsaad.covidapp.ui.fragments
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.transition.TransitionManager
+import com.aymnsaad.covidapp.R
 import com.aymnsaad.covidapp.databinding.FragmentDetailsBinding
 
 
-class DetailsFragment: BaseFragment<FragmentDetailsBinding>() {
+class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
+
+    private val covidPreventFragment = CovidPreventFragment()
+
     override val LOG_TAG: String = "Info Fragment"
     override val bindingInflater: (LayoutInflater) -> FragmentDetailsBinding
         get() = FragmentDetailsBinding::inflate
@@ -22,15 +27,14 @@ class DetailsFragment: BaseFragment<FragmentDetailsBinding>() {
     }
 
 
+    private fun addExpandAbilty(detailsText: TextView?, readMeView: TextView?) {
 
-    private fun addExpandAbilty(detailsText : TextView?, readMeView : TextView?){
-
-       readMeView?.setOnClickListener {
-           val isExpand = detailsText?.maxLines == Int.MAX_VALUE
+        readMeView?.setOnClickListener {
+            val isExpand = detailsText?.maxLines == Int.MAX_VALUE
             TransitionManager.beginDelayedTransition(binding?.root as ViewGroup)
             detailsText?.maxLines = if (isExpand) 2 else Int.MAX_VALUE
             readMeView?.text = if (isExpand) "Read more" else "Show less"
-       }
+        }
 
     }
 }
--- app/src/main/java/com/aymnsaad/covidapp/ui/fragments/VaccinationFragment.kt
+++ app/src/main/java/com/aymnsaad/covidapp/ui/fragments/VaccinationFragment.kt
@@ -4,7 +4,7 @@ import android.view.LayoutInflater
 import com.aymnsaad.covidapp.data.DataManger
 import com.aymnsaad.covidapp.databinding.FragmentVaccinationBinding
 
-class VaccinationFragment: BaseFragment<FragmentVaccinationBinding>() {
+class VaccinationFragment : BaseFragment<FragmentVaccinationBinding>() {
     override val LOG_TAG: String = "Vaccination Activity"
 
     override val bindingInflater: (LayoutInflater) -> FragmentVaccinationBinding
@@ -16,7 +16,7 @@ class VaccinationFragment: BaseFragment<FragmentVaccinationBinding>() {
         val listOfCountry = DataManger.getTotalVaccinationForAllCountries().toList()
         for (i in listOfCountry.indices) {
             counter++
-            binding?.textview?.append((counter.toString() + "." + listOfCountry[i].first?.take(20)+"\t\t\t \b"+ listOfCountry[i].second +"\n"))
+            binding?.textview?.append((counter.toString() + "." + listOfCountry[i].first?.take(20) + "\t\t\t \b" + listOfCountry[i].second + "\n"))
 
         }
     }
