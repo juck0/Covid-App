481ed4cea90eb83717289b106ee39ead34427ebd
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-11-23 16:59:44 +0300

    add search fragment

--- app/src/main/java/com/aymnsaad/covidapp/data/DataManger.kt
+++ app/src/main/java/com/aymnsaad/covidapp/data/DataManger.kt
@@ -5,7 +5,7 @@ import com.example.covidapple.data.domain.Vaccine
 object DataManger {
 
     private val vaccineList = mutableListOf<Vaccine>()
-
+    private var countryList = mutableListOf<Vaccine>()
     /**
      * This function fill the list of Vaccine
      */
@@ -51,5 +51,45 @@ object DataManger {
         Pair(it.date, it.dailyVaccinations)
     }.toList().sortedBy { (_,v) -> v }.toMap()
 
+    fun getCountry(country: String) = vaccineList.let {
+        countryList = it.filter { it1 ->
+            it1.country?.lowercase()?.trim() == country.lowercase().trim()
+        }.toMutableList()
 
+        it.associateBy(
+            keySelector = { country.lowercase().trim() },
+            valueTransform = { countryList }
+        )
+    }
+    // initCountryList filter vaccineList according to country to create new list content only last row of data
+
+    // function to convert big numbers to K , M and B style
+    fun convertNumber(number: Double): String {
+        return when {
+            (number / 1000000000) >= 1 -> {
+                val number1 =number / 1000000000
+                String.format("%.3f", number1) + "B"
+            }
+            (number / 1000000) >= 1 -> {
+                val number1 =number / 1000000
+                String.format("%.2f", number1) + "M"
+            }
+            (number / 1000) >= 1 -> {
+                ((number / 1000).toInt()).toString() + "K"
+            }
+            else -> {
+                number.toString()
+            }
+        }
+    }
+
+    fun getDailyVaccine(country: String): MutableList<Double> {
+        val list = mutableListOf<Double>()
+        vaccineList.forEach {
+            if (it.country?.lowercase()?.trim() == country.lowercase().trim()){
+                it.dailyVaccinations?.let { it1 -> list.add(it1.toDouble()) }
+            }
+        }
+        return list
+    }
 }
